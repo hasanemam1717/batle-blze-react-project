9922ba3c634817aa04b2b4a5e6a2bf6fa2855df9
hasanemam1717 <hasanemam1717@gmail.com>
2024-03-21 17:28:31 +0600

    added react project for work

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+<button className="btn btn-primary">Primary</button>
+        
+        
+    </>
+  )
+}
+
+export default App
